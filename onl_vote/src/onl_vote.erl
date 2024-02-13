65bf6851a5efbffc3bb668bd6abffc0ec45ca96a
Erick <moralerick90@gmail.com>
2024-02-12 19:25:47 -0800

    edited main file

--- onl_vote/src/onl_vote.erl
+++ onl_vote/src/onl_vote.erl
@@ -1,21 +1,36 @@
 -module(onl_vote).
+-export([start/0, init_vote/1, cast_vote/2, tally_votes/0, vote/2, tally/1]).
 
--export([start/0]).
+% Start the voting system
+start() ->
+    spawn(?MODULE, vote, []).
 
+% Initiate a new vote with a given question
+init_vote(Question) ->
+    VotePid = spawn(fun() -> vote(Question, []) end),
+    VotePid.
 
-start_adder()->
-    spawn(?MODULE, adder, []).
+% Cast a vote for a given option in a vote process
+cast_vote(VotePid, Option) ->
+    VotePid ! {self(), Option}.
 
-adder()->
-    recieve
-        {Pid, N1, N2}->
-            Pid ! N1+N2
-    end,
-    adder().
+% Tally the votes collected in a vote process
+tally_votes() ->
+    tally([]).
 
-add(Pid, N1, N2)->
-    Pid ! {self(), N1, N2},
-    recieve
-        Response->
-            Response
+% Internal function to manage the voting process
+vote(Question, Votes) ->
+    receive
+        {Voter, Option} ->
+            NewVotes = [{Voter, Option} | Votes],
+            vote(Question, NewVotes);
+        tally ->
+            io:format("Tally for vote: ~p~n", [Votes]),
+            tally(Votes)
     end.
+
+% Tally the votes
+tally(Votes) ->
+    % Here you can perform the tallying logic
+    % For demonstration purposes, we simply print out the votes
+    io:format("Final Tally: ~p~n", [Votes]).
