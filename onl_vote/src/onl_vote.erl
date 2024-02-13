749ff9fe05b02ff153ef8d0f75f904281b7f6509
Erick <mor16028@byui.edu>
2024-02-12 18:08:13 -0800

    edited file

--- onl_vote/src/onl_vote.erl
+++ onl_vote/src/onl_vote.erl
@@ -1,3 +1,21 @@
 -module(onl_vote).
 
--export([]).
+-export([start/0]).
+
+
+start_adder()->
+    spawn(?MODULE, adder, []).
+
+adder()->
+    recieve
+        {Pid, N1, N2}->
+            Pid ! N1+N2
+    end,
+    adder().
+
+add(Pid, N1, N2)->
+    Pid ! {self(), N1, N2},
+    recieve
+        Response->
+            Response
+    end.
