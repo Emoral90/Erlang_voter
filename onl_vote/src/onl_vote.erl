5acc38ee6a394a0a675eeefe3e943026bf833b23
Erick <moralerick90@gmail.com>
2024-02-16 12:06:23 -0800

    added client functions

--- onl_vote/src/onl_vote.erl
+++ onl_vote/src/onl_vote.erl
@@ -26,6 +26,42 @@ cast_vote(Voter_id, Candidate_id, Votes_Pid)->
             Result
     end.
 
+% Client functions
+
+registered_voters(Registered_voters)->
+    receive
+        {From, {register_voter, Voter_name}}->
+            Voter_id = make_ref(),
+            New_reg_voters = [{Voter_id, Voter_name} | Registered_voters],
+            From ! {selft(), Voter_id},
+            registered_voters(New_reg_voters);
+        _->
+            registered_voters(Registered_voters);
+    end.
+
+candidates(Candidates)->
+    receive
+        {From, {add_candidate, Candidate_name}}->
+            Candidate_id = make_ref(),
+            New_cands = [{Candidate_id, Candidate_name} | Candidates],
+            From ! {self(), Candidate_id},
+            candidates(New_cands);
+        {From, get_candidates}->
+            From ! {self(), Candidates},
+            candidates(Candidates)
+    end.
+
+votes(Votes)->
+    receive
+        {From, {cast_vote, Voter_id, Candidate_id}}->
+            New_votes = [{Voter_id, Candidate_id} | Votes],
+            From ! ok,
+            votes(New_votes);
+        {From, tally_votes}->
+            Total = tally_votes(Votes),
+            votes(Votes)
+    end.
+
 % Count votes and return the results
 tally_votes()->
     io:format("Tallying votes...~n"),
